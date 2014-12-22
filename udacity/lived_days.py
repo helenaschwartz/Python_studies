07d824902cbc5ee3ee7b7b6c2032b5987a9688c7
Helena Schwartz <helena.o.schwartz@gmail.com>
2014-12-22 00:54:13 -0200

    It has worked :)

--- udacity/lived_days.py
+++ udacity/lived_days.py
@@ -1,16 +1,24 @@
-from datetime import date
-from datetime import datetime
+#from datetime import date
+#from datetime import datetime
 
 #today = datetime.today()
 
-today_writed = date(2014,12,22)
-birth_date = date(1985,05,17)
+#today_writed = date(2014,12,22)
+#birth_date = date(1985,05,17)
 
  
-diff = today_writed - birth_date
-print diff.days
+#diff = today_writed - birth_date
+#print diff.days
 
 #Still need to understand how to transform the datetime.today only on days to be possible to do the difference
 
     
 
+import datetime
+from datetime import date
+
+today = datetime.date.today()
+birth_date = date(1985,05,17)
+
+diff = today - birth_date
+print diff.days
